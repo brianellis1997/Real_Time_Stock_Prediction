e5eef41b0c1a9ce2a46f627f69d4b127c437aa12
Brian Ellis <brianellis1997@gmail.com>
2024-07-24 09:16:48 -0400

    added multiple stocks and exported to dataframe

--- webscrapping.py
+++ webscrapping.py
@@ -51,5 +51,23 @@ def real_time_price(stock_code):
     return price, change, volume, latest_pattern, one_year_target
 
 
-Stock = ['BRK-B']
-print(real_time_price('BRK-B'))
+Stock = ['BRK-B', 'PYPL', 'TWTR', 'AAPL', 'AMZN', 'MSFT', 'META', 'GOOG']
+
+while (True):
+    info = []
+    col = []
+    time_stamp = datetime.datetime.now()
+    time_stamp = time_stamp.strftime('%Y-%m-%d %H:%M:%S')
+    for stock_code in Stock:
+        stock_price, change, volume, latest_pattern, one_year_target = real_time_price(stock_code)
+        info.append(stock_price)
+        info.extend([change])
+        info.extend([volume])
+        info.extend([latest_pattern])
+        info.extend([one_year_target])
+    col =[time_stamp]
+    col.extend(info)
+    df = pd.DataFrame(col)
+    df = df.T
+    df.to_csv(str(time_stamp[0:11]) + 'stock_data.csv', mode = 'a', header = False)
+    print(col)
